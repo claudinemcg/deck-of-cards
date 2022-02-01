8f1cf98d2fa898fc8e11d69e694028b9111e5485
Claudine McGowan <claudine.mcgowan@gmail.com>
2022-02-01 13:27:43 +0000

    added deck of cards api

--- src/Deck.js
+++ src/Deck.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
+const API_BASE_URL = 'https://www.deckofcardsapi.com/api/deck/
+const shuffle = '
 
 class Deck extends Component {
     constructor(props) {
         super(props)
-        this.state = { imgUrl: "", name: "" }
+        this.state = { deck: null }
+        this.getCard = this.getCard.bind(this);
     }
 
-    render() {
+    
+    async componentDidMount() {
+        let response = await axios.get(`${API_BASE_URL}new/shuffle`);
+        console.log(response.data)
+        this.setState({deck: response.data})
+    }
+
+    getCard() {
+        https://www.deckofcardsapi.com/api/deck/
+        // make request using deck_id
+        let request = await axios.get(`${API_BASE_URL}${deck.deck_id}/draw/}`);
+        // set state using new card info
+    }
+        render() {
         return (
             <div>
                 <h1>Card Dealer</h1>
-                {/* <img src={this.state.imgUrl} /> */}
-                {/* <h2>{this.state.name}</h2> */}
+                <button onClick={this.getCard}>Get Card!</button>
             </div>
         )
     }

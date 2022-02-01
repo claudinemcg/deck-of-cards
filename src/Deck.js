72577722552b3f4d6c24979e4c7bcaf36bab965b
Claudine McGowan <claudine.mcgowan@gmail.com>
2022-02-01 15:42:48 +0000

    using api data to get card

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-const API_BASE_URL = 'https://www.deckofcardsapi.com/api/deck/
-const shuffle = '
+const API_BASE_URL = 'https://www.deckofcardsapi.com/api/deck'
 
 class Deck extends Component {
     constructor(props) {
@@ -12,16 +11,29 @@ class Deck extends Component {
 
     
     async componentDidMount() {
-        let response = await axios.get(`${API_BASE_URL}new/shuffle`);
-        console.log(response.data)
-        this.setState({deck: response.data})
+        let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`);
+        console.log(deck.data)
+        this.setState({deck: deck.data})
     }
 
-    getCard() {
-        https://www.deckofcardsapi.com/api/deck/
+    async getCard() {
         // make request using deck_id
-        let request = await axios.get(`${API_BASE_URL}${deck.deck_id}/draw/}`);
+        let id = this.state.deck.deck_id
+        let cardUrl = `${API_BASE_URL}/${id}/draw/`;
+        let cardResponse = await axios.get(cardUrl);
+        console.log(cardResponse.data)
         // set state using new card info
+        let card = cardResponse.data.cards[0] //in data
+        this.setState(st => ({
+            drawn: [
+                ...st.drawn, // what's already in state
+                {
+                    id: card.code,
+                    image: card.image,
+                    name: `${card.suit} ${card.value}`
+                }
+            ]
+        }));
     }
         render() {
         return (
